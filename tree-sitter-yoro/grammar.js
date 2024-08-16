891603fd904d2bd73aaa2d8aaa55fee7d88d8ca9
Timi <ttaiwo73@gmail.com>
2024-08-16 10:49:28 +0100

    grammar updates

--- tree-sitter-yoro/grammar.js
+++ tree-sitter-yoro/grammar.js
@@ -6,7 +6,8 @@ module.exports = grammar({
 
     source_file: $ => repeat(choice($._primitive, $.identifier)),
 
-    _grapheme: $ => token(/[a-zA-ZẹọṣẸỌṢ_]+/), 
+    _grapheme: $ => token(/[a-zA-ZẹọṣẸỌṢ_]/), 
+    identifier: $ => token(/[a-zA-zẹọṣẸỌṢ_][a-zA-zẹọṣẸỌṢ_0-9]+/),
     
     /* Update to represent 
     
@@ -21,13 +22,10 @@ module.exports = grammar({
     _decimal_digit: $ => token(/[0-9]/),
     _hexadecimal_digit: $ => /[0-9a-fA-F]/,
 
-    // identifier: $ => seq($._grapheme, repeat(choice($._grapheme, $._decimal_digit))),
-    identifier: $ => seq(repeat1(choice($._grapheme, $._decimal_digit))),
-
     // _primitive: $ => choice( $.integer, $.floating_point, 
     //                     $.boolean, $.character, $.string),
 
-    _primitive: $ => choice( $.boolean, $.character),
+    _primitive: $ => choice( $.boolean, $.character, $.integer),
 
     // Boolean primitives
     boolean: $ => choice($.true, $.false),
@@ -35,21 +33,20 @@ module.exports = grammar({
     false: _ => 'irọ',
 
     // Integer characters
-    // _positive_or_negative: $ => choice('+', '-'),
+    _positive_or_negative: $ => choice('+', '-'),
 
-    // integer: $ => choice($.binary_integer, $.octal_integer, $.decimal_integer, $.hexadecimal_integer),
-    // floating_point: $ => seq($._decimal_digit, '.', $._decimal_digit),
+    integer: $ => choice($.binary_integer, $.octal_integer, $.decimal_integer, $.hexadecimal_integer),
+    floating_point: $ => seq($._decimal_digit, '.', $._decimal_digit),
 
-    // binary_integer: $ => seq(optional($._positive_or_negative), choice('0b', '0B'), repeat1($._binary_digit)),
-    // octal_integer: $ => seq(optional($._positive_or_negative), choice('0o', '0O'), repeat1($._octal_digit)),
-    // decimal_integer: $ => seq(optional($._positive_or_negative), repeat1($._decimal_digit)),
-    // hexadecimal_integer: $ => seq(optional($._positive_or_negative), choice('0x', '0X'), repeat1($._hexadecimal_digit)),
+    binary_integer: $ => seq(optional($._positive_or_negative), choice('0b', '0B'), repeat1($._binary_digit)),
+    octal_integer: $ => seq(optional($._positive_or_negative), choice('0o', '0O'), repeat1($._octal_digit)),
+    decimal_integer: $ => seq(optional($._positive_or_negative), repeat1($._decimal_digit)),
+    hexadecimal_integer: $ => seq(optional($._positive_or_negative), choice('0x', '0X'), repeat1($._hexadecimal_digit)),
 
 
     character: $ => seq('\'', $._grapheme, '\''),
     // string: $ => seq('\"', repeat(choice($._grapheme, $.integer)), '\"'),
     string: $ => seq('\"', repeat($._grapheme), '\"'),
-
 
   }
 });
