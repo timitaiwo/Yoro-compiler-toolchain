bed8cfec81fc196e60b6d3fe2b648ba11702beb3
Timi Taiwo <timi_taiwo@protonmail.com>
2024-10-02 11:27:23 +0100

    prepare for templates

--- setup.js
+++ setup.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 
 let child = require('child_process');
+let isWindows = process.platform == 'win32';
 
-let altervativeBuildTaken = false;
+let altervativePathTaken = false;
 const initialList = ["cmake", "tree-sitter"];
 
+// Function to check if binaries in the list are available on the host operating system
 let checkInPath = (binaryList) => {
 
     for (let i = 0; i < binaryList.length; i++){
         let binary = binaryList[i];
 
-        let whichCommand = process.platform == 'win32'? 'where.exe' : 'which';
+        let whichCommand = isWindows ? 'where.exe' : 'which';
         
         let binaryExists = child.spawnSync(whichCommand, [binary]).status === 0;
 
@@ -30,10 +32,11 @@ if (checkInPath(initialList)){
     console.log("All binaries needed on OS Path found\n");
 }
 else {
-    console.log(`Missing binary on OS path from list ${initialList}`)
+    console.log(`Missing binary on OS path from list ${initialList}, checking alternatives`)
     let alternative_list = ["cmake", "rustc", "cargo"];
 
     if (checkInPath(alternative_list)){
+        altervativePathTaken = true;
         console.log(`altervative build path found\n`)
     } else {
         console.log(`Programs needed for this project to be built are missing.`)
@@ -44,6 +47,6 @@ else {
 
 
 // Template generation
-console.log("Starting grammar.js generation")
+console.log("Starting template generation")
 
 
