4fc31c528343765de9994cac2112c677813a2003
Timi Taiwo <ttaiwo73@gmail.com>
2026-07-22 12:02:15 +0100

    initial nix flake command

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "Yọrọ complier toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+}
